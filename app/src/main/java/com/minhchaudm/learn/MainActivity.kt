0a91fe3f5d3eb6120bfbf4fe3eb8fe91856069a4
Đào Minh Châu <daominhchau18@gmail.com>
2020-04-07 09:20:08 +0700

    7/4/2020

--- app/src/main/java/com/minhchaudm/learn/MainActivity.kt
+++ app/src/main/java/com/minhchaudm/learn/MainActivity.kt
@@ -9,33 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        var a : String = "chau"
-//            a = " hien"
-//        val b: Int = 10
-//        Log.d("AAA","da tim thay " + a + b)
-//
-//        var a: Int = 0
-//        for (i in 1..10 step 2){
-//            Log.d("BBB","thuc hien" + i)
-//        }
-//        checknumber(5)
-//        var mang : Array<Int> = arrayOf(1,2,3,4);
-//        for (number in mang){
-//            Log.d("AAA","Number in array: "+ number)
-//            Log.d("AAA", mang.size.toString())
-//        }
-
-        var sv1 : Sinhvien = Sinhvien()
-        sv1.HoTen = " Dao Minh Chau"
-        sv1.DiaChi = " Quang Ngai"
-        sv1.setNamsinh(2003)
-        Log.d("BBB","Sinh vien ten"+ sv1.HoTen + " nam sinh " + sv1.getNamsinh() + "-" + sv1.tinhtuoi())
+        var a  = "welcome to kotlin"
+        Log.d("AAA","Hello" + a)
     }
-//    fun  checknumber ( innumber : Int)= when(innumber){
-//       0 -> Log.d("AAA","number is zero")
-//       1,2 -> Log.d("AAA","one or two")
-//       in 3..9 -> Log.d("AAA","big")
-//       else -> Log.d("AAA","default")
-//        }
 
 }
